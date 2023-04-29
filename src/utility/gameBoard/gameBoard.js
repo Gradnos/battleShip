512472d279c3dfab2f13561d2741f9038eb6c83e
Gradnos <sdolaqidze@gmail.com>
2023-04-29 12:31:05 +0400

    Add PlaceShip and canPlaceShip functions to gameboard

--- src/utility/gameBoard/gameBoard.js
+++ src/utility/gameBoard/gameBoard.js
@@ -7,10 +7,35 @@ export class GameBoard{
         this.shipArray = new Array();
         this.freeSpaces = coordObjects2D(size);
         
-
-        
     }
 
+    canPlaceShip(ship){
+        let shipSpaces = [{x:ship.x, y:ship.y}];
+        let orientation = ship.orientation;
+        if(orientation === 1){p = -1; q = 0;}
+        if(orientation === 2){p = 0; q = 1;}
+        if(orientation === 3){p = 1; q = 0;}
+        if(orientation === 4){p = 0; q = -1;}
 
+        for(let i = 1; i < ship.size; i++) shipSpaces.push({x : ship.x + q * i, y : ship.y + p * i});
+
+        this.shipArray.forEach(ship => {
+            shipSpaces.forEach(space => {
+                if(ship.isOnCoords(space.x, space.y)) return false;
+            });
+        });
+        return true;
+    }
+
+    placeShip(ship){
+        if(!this.canPlaceShip(ship)) return;
+
+        this.shipArray.push(ship);
+
+
+
+
+
+    }
 
 }
